e9592c8939c97f18ff334d58fc015df4216ab43e
psrth <parthsharma151@gmail.com>
2022-05-09 05:38:17 +0530

    edit: minor styling and consistency fixes in layout and nav, linking, and copy edits

---
+++ data/home.js
@@ -0,0 +1,27 @@
+export const readingList = [
+    {
+      id: 1,
+      title: "Essentialism",
+      author: "Greg McKeown"
+    },
+    {
+      id: 2,
+      title: "Trillion Dollar Coach",
+      author: "Eric Schmidt"
+    },
+    {
+      id: 3,
+      title: "Zero to One",
+      author: "Peter Thiel"
+    },
+    {
+      id: 4,
+      title: "Psychology of Money",
+      author: "Morgan Housel"
+    },
+    {
+      id: 5,
+      title: "Phantasmal Media",
+      author: "Dr. Fox Harrell"
+    },
+  ]
