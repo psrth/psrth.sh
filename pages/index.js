9daab2d6a7eeefb2261d5625ed76298094cdfe06
psrth <parthsharma151@gmail.com>
2022-05-15 03:26:23 +0530

    add: routing basic fixes, misc animations

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,7 @@ import styles from '../styles/Home.module.css'
 
 
 export default function Home() {
+
   return (
     <div className={styles.master}>
       <Head>
---
+++ pages/portfolio.js
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+
+import Divider from '../components/Global/Divider';
+import Navbar from '../components/Global/Navbar';
+import Footer from '../components/Global/Footer';
+
+import TextSection from '../components/Home/TextSection';
+import styles from '../styles/Home.module.css'
+
+
+export default function Portfolio() {
+
+  return (
+    <div className={styles.master}>
+      <Head>
+        <title>Parth Sharma</title>
+        <meta name="description" content="The website and blog of Parth Sharma, a developer, designer, builder and sophomore at BITS Pilani." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className={styles.card}>
+
+        <Navbar />
+        <Divider />
+
+        <TextSection
+          heading="Portfolio"
+          text="Coming soon!"
+        />
+
+        <Divider />
+        <Footer />
+      </div>
+    </div>
+  )
+}
