61403412b42130360547499dc327cc6cc9ad32f2
psrth <parthsharma151@gmail.com>
2023-09-12 18:18:10 +0530

    ⚒️ (bug) routing bug fix

--- pages/bits.js
+++ pages/bits.js
@@ -7,7 +7,7 @@ import Footer from "../components/Global/Footer";
 import TextSection from "../components/Home/TextSection";
 import styles from "../styles/Home.module.css";
 
-export default function BITS() {
+export default function Portfolio() {
   return (
     <div className={styles.master}>
       <Head>
