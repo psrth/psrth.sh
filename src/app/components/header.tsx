97e70617d10b3328ead08f33e0f2f27485db916e
psrth <parthsharma151@gmail.com>
2025-05-11 18:02:14 -0700

    feat(ui): added path matching styling to header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <div className="flex flex-col w-full font-soehne">
       <div className="bg-[var(--color-teal)] h-4 w-full" />
@@ -12,27 +17,47 @@ export default function Header() {
             <div className="text-md font-medium">psrth</div>
           </div>
         </Link>
-        <div className="hidden md:flex flex-row items-center gap-4 mt-10">
-          <Link href="/">
-            <div className="text-md font-medium cursor-pointer">home</div>
-          </Link>
-          <Link href="/projects">
-            <div className="text-md text-[var(--color-light-gray)] font-regular cursor-pointer">
-              projects
-            </div>
-          </Link>
-          <Link href="/notes">
-            <div className="text-md text-[var(--color-light-gray)] font-regular cursor-pointer">
-              notes
-            </div>
-          </Link>
-          <Link href="/contact">
-            <div className="text-md text-[var(--color-light-gray)] font-regular cursor-pointer">
-              contact
-            </div>
-          </Link>
+        <div className="hidden md:flex flex-row items-center mt-10">
+          <HeaderItem href="/" label="home" currentPath={pathname} />
+          <HeaderItem
+            href="/projects"
+            label="projects"
+            currentPath={pathname}
+          />
+          <HeaderItem href="/notes" label="notes" currentPath={pathname} />
+          <HeaderItem href="/contact" label="contact" currentPath={pathname} />
         </div>
       </div>
     </div>
+  );
+}
+
+interface HeaderItemProps {
+  href: string;
+  label: string;
+  currentPath: string;
+}
+
+function HeaderItem({ href, label, currentPath }: HeaderItemProps) {
+  const isActive = currentPath === href;
+
+  return (
+    <Link
+      href={href}
+      prefetch={true}
+      className={`px-3 py-1 cursor-pointer ${
+        isActive ? "bg-gray-50" : "bg-transparent"
+      }`}
+    >
+      <p
+        className={`text-md ${
+          isActive
+            ? "text-[var(--color-gray)] font-medium"
+            : "text-[var(--color-light-gray)] font-regular"
+        }`}
+      >
+        {label}
+      </p>
+    </Link>
   );
 }
